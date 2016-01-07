8dcf97724113e12a35844cb9536a4375b114d04b
Samiul Anwar <samiul.anwar@gmail.com>
2016-01-07 12:22:08 -0500

    just saving my work

--- 09_timer/test.rb
+++ 09_timer/test.rb
@@ -10,3 +10,24 @@ class Timer
 		total_time="#{seconds}:#{minutes}:#{hours}"
 	end
 end
+================================================
+class Timer
+	attr_accessor :seconds
+	def seconds
+		@seconds=0
+	end
+	def time_string
+		seconds=@seconds
+		minutes=0
+		hours=0
+		if seconds>=60
+			temp_seconds=seconds
+			while temp_seconds>=60
+				temp_seconds=temp_seconds-60
+				minutes=minutes+1
+			end
+			seconds=temp_seconds
+		end
+		total_time="#{hours}:#{minutes}:#{seconds}"
+	end
+end
--- 09_timer/timer.rb
+++ 09_timer/timer.rb
@@ -4,9 +4,17 @@ class Timer
 		@seconds=0
 	end
 	def time_string
-		seconds=0
+		seconds=@seconds
 		minutes=0
 		hours=0
-		total_time="#{seconds}:#{minutes}:#{hours}"
+		if seconds>=60
+			temp_seconds=seconds
+			while temp_seconds>=60
+				temp_seconds=temp_seconds-60
+				minutes=minutes+1
+			end
+			seconds=temp_seconds
+		end
+		total_time="#{hours}:#{minutes}:#{seconds}"
 	end
 end
